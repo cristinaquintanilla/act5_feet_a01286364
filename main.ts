f7a8a1d84c0e8866aa1f840e68acc016f180e40f
cristinaquintanilla <70345076+cristinaquintanilla@users.noreply.github.com>
2020-09-10 15:11:46 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    yards = feet / 3
+    basic.showNumber(yards)
+    basic.showString("Yards")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    inches = feet * 12
+    centimeters = inches * 2.54
+    basic.showNumber(centimeters)
+    basic.showString("Centimeters")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    inches = feet * 12
+    basic.showNumber(inches)
+    basic.showString("Inches")
+})
+input.onGesture(Gesture.Shake, function () {
+    let meters = 0
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    inches = feet * 12
+    centimeters = inches * 2.54
+    basic.showNumber(meters)
+    basic.showString("Meters")
+})
+let centimeters = 0
+let inches = 0
+let yards = 0
+let feet = 0
+feet = randint(1, 100)
